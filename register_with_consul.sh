046975f08566ce1aa1f7fa47336ad834e57865be
Pritam Kadam <phkadam2008@gmail.com>
2018-05-24 15:56:10 +0530

    Remove consul

--- register_with_consul.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-http PUT http://54.88.52.159:8500/v1/agent/service/register < perf1.json
-http PUT http://54.88.52.159:8500/v1/agent/service/register < perf2.json
-http PUT http://54.88.52.159:8500/v1/agent/service/register < perf3.json
-http PUT http://54.88.52.159:8500/v1/agent/service/register < perf4.json
-http PUT http://54.88.52.159:8500/v1/agent/service/register < perf5.json
